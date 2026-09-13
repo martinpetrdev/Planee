7ddeb48d55f74aaba3aced7f102be7476826057c
Martin Petr <martinpetrnp@gmail.com>
2026-09-13 11:26:27 +0200

    feat(mobile): add icon, splash screen

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -18,31 +18,27 @@ export default {
     icon: "./assets/images/icon.png",
     scheme: scheme,
     userInterfaceStyle: "automatic",
-    ios: {
-      icon: "./assets/expo.icon",
-    },
     android: {
       package: packageName,
       adaptiveIcon: {
-        backgroundColor: "#E6F4FE",
+        backgroundColor: "#FFFFFF",
         foregroundImage: "./assets/images/android-icon-foreground.png",
-        backgroundImage: "./assets/images/android-icon-background.png",
         monochromeImage: "./assets/images/android-icon-monochrome.png",
       },
       predictiveBackGestureEnabled: false,
-    },
-    web: {
-      output: "static",
-      favicon: "./assets/images/favicon.png",
     },
     plugins: [
       "expo-router",
       [
         "expo-splash-screen",
         {
-          backgroundColor: "#208AEF",
-          image: "./assets/images/splash-icon.png",
-          imageWidth: 76,
+          backgroundColor: "#FFFFFF",
+          image: "./assets/images/splash-light.png",
+          imageWidth: 128,
+          dark: {
+            backgroundColor: "#121318",
+            image: "./assets/images/splash-dark.png",
+          },
         },
       ],
       "expo-status-bar",
