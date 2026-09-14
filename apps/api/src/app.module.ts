868c37439bac1844e07e107716d25e8a37f3fc00
Martin Petr <martinpetrnp@gmail.com>
2026-09-14 09:25:02 +0200

    chore(api): remove debug endpoint

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './health/health.module.js';
 import { ConfigModule } from './config/config.module.js';
-import { SessionModule } from './session/session.module.js';
 import { AuthModule } from './shared/auth/auth.module.js';
 
-const DomainModules = [HealthModule, SessionModule];
+const DomainModules = [HealthModule];
 
 @Module({
   imports: [ConfigModule, AuthModule, ...DomainModules],
--- apps/api/src/session/session.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SessionController } from './presentation/session.controller.js';
-
-@Module({
-  imports: [],
-  controllers: [SessionController],
-})
-export class SessionModule {}
