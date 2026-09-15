799272b2e1f46eb10732b5e2e86efdad74e58565
Martin Petr <martinpetrnp@gmail.com>
2026-09-15 15:26:31 +0200

    feat(api): add swagger and helmet

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,9 +6,22 @@ import {
 } from './config/http.config.js';
 import { VersioningType } from '@nestjs/common';
 import { ApiVersion } from '@repo/shared';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Planee API')
+    .setVersion('1.0')
+    .build();
+
+  const swaggerDocumentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, swaggerDocumentFactory);
 
   app.enableShutdownHooks();
   app.enableCors({
