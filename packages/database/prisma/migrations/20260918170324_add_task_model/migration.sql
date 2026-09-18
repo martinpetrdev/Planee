68b9811e05a07290ddd87dc2f07f3846b8caacd2
Martin Petr <martinpetrnp@gmail.com>
2026-09-18 19:04:19 +0200

    feat(database): add task model

---
+++ packages/database/prisma/migrations/20260918170324_add_task_model/migration.sql
@@ -0,0 +1,22 @@
+-- CreateEnum
+CREATE TYPE "TaskPriority" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "expectedDuration" INTEGER NOT NULL,
+    "dueDate" TIMESTAMP(3) NOT NULL,
+    "priority" "TaskPriority" NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Task_userId_idx" ON "Task"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
