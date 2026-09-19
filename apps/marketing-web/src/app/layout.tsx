efc36db9b983b598e30281b39a89373f757e9e47
Martin Petr <martinpetrnp@gmail.com>
2026-09-19 20:56:12 +0200

    chore: add marketing webpage

---
+++ apps/marketing-web/src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Figtree, Lora } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const loraHeading = Lora({ subsets: ["latin"], variable: "--font-heading" });
+
+const figtree = Figtree({ subsets: ["latin"], variable: "--font-sans" });
+
+export const metadata: Metadata = {
+  title: "Planee",
+  description: "Day planner for people who love forgetting things",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html
+      lang="en"
+      className={cn(
+        "dark",
+        "h-full",
+        "antialiased",
+        "font-sans",
+        figtree.variable,
+        loraHeading.variable,
+      )}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
